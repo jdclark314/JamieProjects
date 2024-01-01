5daecd64964825bded5418e7e4016d2be585ed84
jdclark905 <76894714+jdclark905@users.noreply.github.com>
2024-01-01 12:57:54 -0800

    improve test coverage

--- Larger Projects/AirplaneSim/dataprocessing/dataProcess_test.go
+++ Larger Projects/AirplaneSim/dataprocessing/dataProcess_test.go
@@ -1,7 +1,6 @@
 package dataprocessing
 
 import (
-	"fmt"
 	"sim/passenger"
 	"testing"
 
@@ -9,7 +8,6 @@ import (
 )
 
 func Test(t *testing.T) {
-	fmt.Println("We in the test yo!")
 	passengers := []passenger.Passenger{
 		{
 			AisleTravelSpeed:  0,
@@ -31,4 +29,66 @@ func Test(t *testing.T) {
 	sut := DataProcess(passengers)
 	assert.Equal(t, float32(13), sut.totalMetrics.avgTimeToBoard)
 	// t.Fail()
+}
+func TestCalcPassengerData(t *testing.T) {
+	p := passenger.Passenger{
+		AisleTravelSpeed:  0,
+		TimeBoardedPlane:  10,
+		TimeSatDown:       16,
+		Seat:              "",
+		CurrentPOS:        0,
+		TimeSinceLastMove: 0,
+	}
+
+	totData := TotalData{}
+	totData.calcPassengerData(p)
+
+	expectedData := passengerData{
+		PassengerDetails: p,
+		PassengerMetrics: passengerMetrics{
+			timeToBoard: p.TimeSatDown - p.TimeBoardedPlane,
+		},
+	}
+
+	assert.Equal(t, []passengerData{expectedData}, totData.PassengerList)
+}
+func TestCalcTotalMetrics(t *testing.T) {
+	passengerList := []passengerData{
+		{
+			PassengerDetails: passenger.Passenger{
+				AisleTravelSpeed:  0,
+				TimeBoardedPlane:  10,
+				TimeSatDown:       16,
+				Seat:              "",
+				CurrentPOS:        0,
+				TimeSinceLastMove: 0,
+			},
+			PassengerMetrics: passengerMetrics{
+				timeToBoard: 6,
+			},
+		},
+		{
+			PassengerDetails: passenger.Passenger{
+				AisleTravelSpeed:  0,
+				TimeBoardedPlane:  10,
+				TimeSatDown:       30,
+				Seat:              "",
+				CurrentPOS:        0,
+				TimeSinceLastMove: 0,
+			},
+			PassengerMetrics: passengerMetrics{
+				timeToBoard: 20,
+			},
+		},
+	}
+
+	totalData := TotalData{
+		PassengerList: passengerList,
+		totalMetrics:  totalMetrics{},
+	}
+
+	totalData.calcTotalMetrics()
+
+	expectedAvgTimeToBoard := float32(13)
+	assert.Equal(t, expectedAvgTimeToBoard, totalData.totalMetrics.avgTimeToBoard)
 }
